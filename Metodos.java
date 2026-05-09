5130e1e62a2a2155ee91f60b4524a95ec3002ba4
Diego Ortiz <diegoalejandroortiz@ucompensar.edu.co>
2026-05-09 18:53:11 -0500

    Añado el primer metodo de saludo al usuario

---
+++ Metodos.java
@@ -0,0 +1,13 @@
+
+public class Metodos {
+
+    // Método que saluda al usuario
+    public static void saludar() {
+        System.out.println("¡Hola! Bienvenido al programa :p.");
+    }
+
+    // Método principal
+    public static void main(String[] args) {
+        saludar();
+    }
+}
